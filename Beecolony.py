530b637320176efd84a1224769d14825aa61c262
kante shiva chandra <35963631+shivachandrakante@users.noreply.github.com>
2019-11-06 22:00:48 +0530

    Minor update in the code

--- Beecolony.py
+++ Beecolony.py
@@ -7,15 +7,15 @@ class Node:
     def __init__(self, idn, x, y):
         self.idn = idn
         self.pos = np.array((float(x), float(y)))
-
-
+        
+        
 class Bee:
     def __init__(self):
         self.choosen_nodes = []
         self.recuiter = True
         self.distance = 0.0
 
-    def choose_rand_move(self, move, nods):
+    def choose_rand_node(self, move, nods):
         # choosen node must be unique
         for i in range(move):
             if self.is_complete():
@@ -59,14 +59,15 @@ def load_nodes(filename):
     nodes_s = [x.strip() for x in nodes_s]
     for n in nodes_s:
         node = n.split(' ')
-        ret.append(Node(node[0], node[1], node[2]))
+        ret.append(Node(node[0], node[1], node[2]))    
     return ret
 
 
-nodes = load_nodes(r"C:\Users\ansul\Desktop\New folder (5)\New folder\datasets\data2")
 
 
-# Bee Colony Optimization Algorithm
+
+nodes = load_nodes(r"C:/Users/Shiva Chandra/Documents/Project/datasets/data")
+
 
 def main():
     epoch = 10
@@ -89,7 +90,7 @@ def main():
         print ("forward pass")
         # forward pass
         for bee in bees:
-            bee.choose_rand_move(n_move, nodes)
+            bee.choose_rand_node(n_move, nodes)
 
         # backward pass
         print ("evaluating")
@@ -98,7 +99,8 @@ def main():
 
         print ("Best distance so far", best_bee.distance)
         print ("Best route so far", [n.idn for n in best_bee.choosen_nodes])
-
+        global route
+        route=[n.idn for n in best_bee.choosen_nodes]
         print ("Bees are making decision to be recruiter or follower")
         Cmax = max(bees, key=lambda b: b.distance).distance
         Cmin = min(bees, key=lambda b: b.distance).distance
@@ -108,7 +110,7 @@ def main():
             Ob = (Cmax - bee.distance) / (Cmax - Cmin)  # range [0,1]
             probs = np.e ** (-(1 - Ob) / (len(bee.choosen_nodes) * 0.01))
             rndm = r.uniform(0, 1)
-            print ("ob and probs", Ob, probs)
+            #print ("ob and probs", Ob, probs)
             if rndm < probs:
                 bee.change_role(True)
                 recruiters.append(bee)
@@ -132,16 +134,16 @@ def main():
                         break
                 bee.replace_nodes(selected_bee.choosen_nodes[:])
         e += 1
-
-
-# sandbox()
+        
+# Bee Colony Optimization Algorithm        
 main()
-# Bee Colony Optimization Algorithm
+
+# Route how the Bees are travelling
 import math
 from IPython import display
 import pylab as pl
 import time
-def sandbox():
+def Route():
     x = [node.pos[0] for node in nodes]
     y = [node.pos[1] for node in nodes]
     l = [node.idn for node in nodes]
@@ -170,17 +172,17 @@ def sandbox():
         display.display(pl.gcf())
         time.sleep(1.0)
         
-    plt.figure(figsize=(20,10))
-    fig, ax = plt.subplots()
-    ax.scatter(x, y)
+
+    fig,ax = plt.subplots(figsize=(20,10))
+    ax.scatter(x, y,s=150,color='red',marker='*')
 
     for i, lbl in enumerate(l):
-        ax.annotate(lbl, (x[i], y[i]))
+        ax.annotate(lbl, (x[i], y[i]),size=20)
 
     
-    plt.show()
+    #plt.show()
 
 
-# sandbox()
 
-sandbox()
+
+Route()
